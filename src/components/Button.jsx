c18702ff74e1bef49a293b1c3cc8e435b0e1fcd5
surya-webdev <surya.webdev23@gmail.com>
2024-06-15 20:38:35 +0530

    feat:added other sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
-function Button({ onClick }) {
+function Button({ onClick, className }) {
   return (
     <button
       onClick={onClick}
-      className="z-0 w-full rounded-lg bg-black px-10 py-3 font-syne text-white"
+      className={`${className} z-0 w-full rounded-lg bg-black px-10 py-3 font-syne text-white`}
       href="#"
     >
       Donate Now
